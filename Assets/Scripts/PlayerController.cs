059c208871eb1a4860c45a9b73c91201d404f18d
jeff john <jeffsams@gmail.com>
2017-06-02 15:38:47 -0400

    Displaying Score and Text
    Edited the script to be able to display the number of counted objects as well as display a win text after collecting all 12 objects

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
+using UnityEngine.UI;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
     private Rigidbody rb;
+    private int count;
 
     public float speed;
+    public Text countText;
+    public Text winText;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
     }
     void Update ()
     {
@@ -32,6 +39,17 @@ public class PlayerController : MonoBehaviour {
         if (other.gameObject.CompareTag("Pick Up")) ;
         {
             other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText ()
+    {
+        countText.text = "count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
         }
     }
 }
